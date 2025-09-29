b995dc7a45d6f2384eb5d3e31d048719b18e4e4e
jfb0301 <bianchi.josefrancisco@gmail.com>
2025-09-29 15:37:55 -0300

    Labels and switch for composite types

--- ch3/labels/labels.go
+++ ch3/labels/labels.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	samples := []string{"Hello", "Apple_π!"}
+	samples := []string{"Hello", "apple_π"}
+outer:
 	for _, sample := range samples {
 		for i, r := range sample {
 			fmt.Println(i, r, string(r))
+			if r == 'l' {
+				continue outer
+			}
 		}
 		fmt.Println()
 	}

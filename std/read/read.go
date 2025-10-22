78c66af788fa9b677a02b5ae052b7f457b0f0835
jfb0301 <bianchi.josefrancisco@gmail.com>
2025-10-22 15:38:59 -0300

    read lib implementation

---
+++ std/read/read.go
@@ -0,0 +1,19 @@
+func countLetters(r io.reader) (map[string]int, error) {
+	buf := make([]byte, 2048)
+	out := map[string]int{}
+	for {
+		n, err := r.Read(buf)
+		for _, b := range buf[:n] {
+			if (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') {
+				out[string(b)]++
+			}
+		}
+		if err == io.EOF {
+			return out, nil 
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+}
+

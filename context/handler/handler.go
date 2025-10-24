ac89e82c8f4a4dfb0e13a9931b42ab9f07413539
jfb0301 <bianchi.josefrancisco@gmail.com>
2025-10-24 16:20:09 -0300

    HTTP handler function implementation

---
+++ context/handler/handler.go
@@ -0,0 +1,19 @@
+func handler(rw http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	err := req.ParseForm()
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte(err.error()))
+		return
+	}
+
+	data := req.FormValue("data")
+	result, err := logic(ctx, data)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.WriteHeader([]byte(err.error()))
+		return 
+	}
+	rw.Write([]byte(result))
+}
+
